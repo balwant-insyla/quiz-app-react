b7f59ef39dbec20cfc226fe6dc93cfb0bd27c421
Balwant Neeb <balwantneeb@Balwants-MacBook-Air.local>
2021-05-27 16:19:49 +0530

    baseURL changes

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://localhost:3000'
-//export const baseURL = 'https://neeb-quiz-app.herokuapp.com'
+//export const baseURL = 'http://localhost:3000'
+export const baseURL = 'https://neeb-quiz-app.herokuapp.com'
